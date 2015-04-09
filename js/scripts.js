ae30c1f41d7795cc2a58ec4814239e9075a0f2ae
Jonathan Lin & Kyle Giard-Chase <student@epicodus.com>
2015-04-09 15:51:04 -0700

    Add functionality to pass test for binary number equal to 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
 var convert = function(input) {
-    return 0;
+    var input_array = input.split('').reverse();
+    var decimal_array = [];
+    var index = 0;
+    var total = 0;
+
+    while(input_array.length > 0) {
+        if( input_array[0] === 1) {
+        decimal_array.push(Math.pow(2, index));
+        input_array.shift();
+        index += 1
+    } else {
+
+    }
+    }
+
+    decimal_array.forEach(function(number) {
+        total += number;
+    });
+    return total;
 }
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,12 @@
 describe('convert', function() {
-    it("it accepts 0 and returns 0", function() {
-        expect(convert(0)).to.equal(0);
-    })
+    // it("it accepts 0 and returns 0", function() {
+    //     expect(convert('0')).to.equal(0);
+    // })
+    it("accepts 1 and returns 1", function() {
+        expect(convert('11111')).to.equal(31);
+    });
+
+    it("accepts 01010 and returns 10", function() {
+        expect(convert('010101')).to.equal(21);
+    });
 });
